348f583973a835b300a8599a307c41b2953b6d01
Bhaskar Upadhyayula <umbs_sairam@yahoo.com>
2018-03-12 09:45:13 -0700

    Remove exporting env variable
    Signed-off-by: Bhaskar Upadhyayula <umbs_sairam@yahoo.com>

--- cscope.sh
+++ cscope.sh
@@ -2,13 +2,10 @@
 echo "----------Remove old cscope files-----------"
 rm -f cscope*
 
-echo "-----  Build cscope.files (.c, .h) ------"
+echo "---------  Build cscope.files -------------"
 # If directories needs to be excluded, add following command
 # '-prune' '-o -type d -name <dir-name> -prune'
 find -L $PWD -name '*.c' -o -name '*.h' -o -name '*.cpp' -o -name '*.py' > cscope.files
 
 echo "-------  Build cscope database -----------"
 cscope -b -q -R
-
-echo "-----  Export CSCOPE_DB env variable ----"
-export CSCOPE_DB=$PWD
