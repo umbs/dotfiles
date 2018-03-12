48b314c07f87a09dc9040365c6a791ead6c86eef
Bhaskar Upadhyayula <umbs_sairam@yahoo.com>
2018-03-12 09:48:01 -0700

    Cosmetic changes to cscope build
    Signed-off-by: Bhaskar Upadhyayula <umbs_sairam@yahoo.com>

--- cscope.sh
+++ cscope.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "----------Remove old cscope files-----------"
+echo "====>Remove old cscope files"
 rm -f cscope*
 
-echo "---------  Build cscope.files -------------"
+echo "====>Build cscope.files"
 # If directories needs to be excluded, add following command
 # '-prune' '-o -type d -name <dir-name> -prune'
 find -L $PWD -name '*.c' -o -name '*.h' -o -name '*.cpp' -o -name '*.py' > cscope.files
 
-echo "-------  Build cscope database -----------"
+echo "====>Build cscope database"
 cscope -b -q -R
