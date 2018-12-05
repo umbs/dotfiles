07eb704fb630dcf50214dbedc4cc8ce9fa900868
Bhaskar S Upadhyayula <bupadhyayula@sjcmacc0kwjhd4.paloaltonetworks.local>
2018-12-05 14:35:19 -0800

    Replace Dropbox with Box
    Signed-off-by: Bhaskar S Upadhyayula <bupadhyayula@sjcmacc0kwjhd4.paloaltonetworks.local>

--- tmux-startup-script.sh
+++ tmux-startup-script.sh
@@ -3,17 +3,19 @@
 tmux start-server
 
 tmux new-session -d -s local -n 'Todo'
-tmux send-keys "vi ~/Dropbox/Todo.txt" C-m
+tmux send-keys "vi -p ~/Box/Todo.txt ~/Box/WorkTodo.txt" C-m
 
 tmux new-window -t local:1 -n 'Journal'
-tmux send-keys "vi ~/Dropbox/Journal.txt" C-m
+tmux send-keys "vi ~/Box/Journal.txt" C-m
 
 tmux new-window -t local:2 -n 'scratch'
-tmux send-keys "vi ~/Dropbox/scratch.txt" C-m
+tmux send-keys "vi ~/Box/scratch.txt" C-m
 
 tmux new-window -t local:3 -n 'blogs'
-tmux send-keys "vi ~/Dropbox/Blog\ Drafts/Misc.txt" C-m
+tmux send-keys "vi ~/Box/Blog\ Drafts/Misc.txt" C-m
 
-tmux new-window -t local:4 -n 'misc'
+tmux new-window -t local:4 -n 'practice'
+
+tmux new-window -t local:5 -n 'misc'
 
 tmux attach -t local
