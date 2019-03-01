d5e28af09eb136dce820dab8b63cf5d294b9a983
Bhaskar S Upadhyayula <umbs_sairam@yahoo.com>
2019-03-01 05:28:24 -0800

    Tmux script to point to right folders
    Signed-off-by: Bhaskar S Upadhyayula <umbs_sairam@yahoo.com>

--- tmux-startup-script.sh
+++ tmux-startup-script.sh
@@ -1,18 +1,19 @@
 #/usr/bin/tmux source-file /path/to/config/file
 
+FOLDER=/c/Users/Dell/FOLDER
+
 tmux start-server
 
 tmux new-session -d -s local -n 'Todo'
-tmux send-keys "vi -p ~/Box/Todo.txt ~/Box/WorkTodo.txt" C-m
+tmux send-keys "vi -p $FOLDER/Todo.txt $FOLDER/WorkTodo.txt" C-m
 
 tmux new-window -t local:1 -n 'Journal'
-tmux send-keys "vi ~/Box/Journal.txt" C-m
+tmux send-keys "vi $FOLDER/Journal.txt" C-m
 
 tmux new-window -t local:2 -n 'scratch'
-tmux send-keys "vi ~/Box/scratch.txt" C-m
+tmux send-keys "vi $FOLDER/scratch.txt" C-m
 
 tmux new-window -t local:3 -n 'blogs'
-tmux send-keys "vi ~/Box/Blog\ Drafts/Misc.txt" C-m
 
 tmux new-window -t local:4 -n 'practice'
 
