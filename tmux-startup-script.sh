2204c95e74366badb82b8fc8d2b50ac706832f9f
Bhaskar Upadhyayula <umbs_sairam@yahoo.com>
2020-03-05 16:25:43 -0800

    Windows renumbering
    Signed-off-by: Bhaskar Upadhyayula <umbs_sairam@yahoo.com>

--- tmux-startup-script.sh
+++ tmux-startup-script.sh
@@ -1,17 +1,17 @@
 #/usr/bin/tmux source-file /path/to/config/file
 
-FOLDER=/c/Users/Dell/FOLDER
+FOLDER=/Users/bupadhyayula/Dropbox
 
 tmux start-server
 
 tmux new-session -d -s local -n 'Todo'
-tmux send-keys "vi -p $FOLDER/Todo.txt $FOLDER/WorkTodo.txt" C-m
+tmux send-keys "vim -p $FOLDER/Todo.txt $FOLDER/WorkTodo.txt" C-m
 
 tmux new-window -t local:1 -n 'Journal'
-tmux send-keys "vi $FOLDER/Journal.txt" C-m
+tmux send-keys "vim $FOLDER/Journal.txt" C-m
 
 tmux new-window -t local:2 -n 'scratch'
-tmux send-keys "vi $FOLDER/scratch.txt" C-m
+tmux send-keys "vim $FOLDER/scratch.txt" C-m
 
 tmux new-window -t local:3 -n 'blogs'
 
