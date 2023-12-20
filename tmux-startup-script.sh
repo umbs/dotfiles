f30f1729521aff25d294ae15e8568209a401cdba
Bhaskar Upadhyayula <umbs_sairam@yahoo.com>
2023-12-20 15:57:42 -0800

    More windows in Tmux bootup script

--- tmux-startup-script.sh
+++ tmux-startup-script.sh
@@ -21,8 +21,8 @@ tmux send-keys "cd $HOME/practice" C-m
 
 tmux new-window -t local:5 -n 'misc'
 
-tmux new-window -t local:5 -n 'code'
-tmux new-window -t local:5 -n 'build'
+tmux new-window -t local:6 -n 'code'
+tmux new-window -t local:7 -n 'build'
 
 tmux attach -t local
 
